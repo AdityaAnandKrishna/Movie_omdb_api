9ddacdc5658cfcc0fffa2bf8b480664e07f0a4e6
Aditya Anand <33352601+AdityaAnandKrishna@users.noreply.github.com>
2021-06-13 15:30:06 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ $(document).ready(() => {
 });
 
 function getMovies(searchText) {
-  axios.get('http://www.omdbapi.com/?apikey=378fa3ec&s=' + searchText)
+  axios.get('https://www.omdbapi.com/?apikey=378fa3ec&s=' + searchText)
     .then((response) => {
       console.log(response);
       let movies = response.data.Search;
@@ -40,7 +40,7 @@ function movieSelected(id) {
 function getMovie() {
   let movieId = sessionStorage.getItem('movieId');
 
-  axios.get('http://www.omdbapi.com?apikey=378fa3ec&i=' + movieId)
+  axios.get('https://www.omdbapi.com?apikey=378fa3ec&i=' + movieId)
     .then((response) => {
       console.log(response);
       let movie = response.data;
